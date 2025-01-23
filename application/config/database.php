5f5eb30197929ad65e4acdee8b347fcc18cc1dae
xaralien <dimasuzumaki126@gmail.com>
2025-01-23 14:21:43 +0700

    Update Major

--- application/config/database.php
+++ application/config/database.php
@@ -73,33 +73,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'tanindo',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => '103.163.138.165',
-// 	'username' => 'lsptanin_admin',
-// 	'password' => 'adminlsptanindo',
-// 	'database' => 'lsptanin_tanindo',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'tanindo',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -115,3 +94,24 @@ $db['default'] = array(
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '103.163.138.165',
+	'username' => 'lsptanin_admin',
+	'password' => 'adminlsptanindo',
+	'database' => 'lsptanin_tanindo',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
